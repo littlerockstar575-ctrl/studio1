c57333031b3c0d0ede7ad57ef9fdc1249a7b82fa
amreek singh <aman81762737@gmail.com>
2025-10-08 17:00:54 +0000

    FirebaseError: Missing or insufficient permissions.

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { firebaseConfig } from '@/firebase/config';
@@ -44,5 +45,6 @@ export * from './provider';
 export * from './client-provider';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
+export * from './auth/use-user';
 export * from './errors';
 export * from './error-emitter';
